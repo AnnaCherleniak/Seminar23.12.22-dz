8dc0fc2465fdc0d9297b6e343bea72856790e032
Anna <ankakras@yandex.ru>
2022-12-27 20:35:13 +0700

    Решение задачи 2

--- Task002.py
+++ Task002.py
@@ -1,10 +1,10 @@
 #2. Задайте натуральное число N. 
 # Напишите программу, которая составит список простых множителей числа N.
-import math
+
 with open('N.txt', 'r', encoding='utf-8') as file:
     n = int(file.readline())
     some_list = []
-    for i in range(2, math.ceil(n**0.5) + 1):
+    for i in range(2, n):
         if n % i == 0:
             some_list.append(i)
             n /= i
