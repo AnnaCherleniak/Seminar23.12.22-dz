de4dba50fc03ac51373bb29c2afe1043d99410ae
Anna <ankakras@yandex.ru>
2022-12-27 17:26:27 +0700

    Решение задачи 1

--- Task001.py
+++ Task001.py
@@ -1,5 +1,12 @@
 #1. Вычислить число c заданной точностью d
 #Пример:
 #- при $d = 0.001, π = 3.141.$    $10^{-1} ≤ d ≤10^{-10}$
-with open('d.txt', 'w', encoding='utf-8') as file:
-    file.write(str(0.001))
+import math
+with open('d.txt', 'r', encoding='utf-8') as file:
+    d = file.readline()
+    for i in range(len(d)):
+        if d[i] == '.':
+            c = len(d) - 1 - i
+    print(round(math.pi, c))
+
+ 
